565e2d738804ccb54311a421653f7e909702488a
Ross Leitch <Ross@metamorphicsolutions.com>
2017-12-09 05:35:20 +1300

    Update dist

--- src/index.js
+++ src/index.js
@@ -58,12 +58,14 @@ class JsPmLoader extends Component {
          },
          map: {
             "react": "react-cdn",
+            "prop-types": 'prop-types-cdn',
             'plugin-babel': 'unpkg:systemjs-plugin-babel@0/plugin-babel.js',
             'systemjs-babel-build': 'unpkg:systemjs-plugin-babel@0/systemjs-babel-browser.js',
             'plugin-babel-remove-props': 'unpkg:babel-plugin-transform-react-remove-prop-types'
          },
          transpiler: 'plugin-babel',
          paths: {
+            'prop-types-cdn': 'https://unpkg.com/prop-types/prop-types.min.js',
             'react-cdn': 'https://unpkg.com/react@15.5.0/dist/react.min.js',
             'unpkg:*' : '//unpkg.com/*',
             'npm:react': 'https://unpkg.com/react@15.5.0/dist/react.min.js',
